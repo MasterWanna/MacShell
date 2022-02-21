139132fdb2e3e48b27fda06753ada8f4c0f337e6
MasterWanna <2467254465@qq.com>
2022-02-22 02:55:10 +0800

    2022-02-22 02:55:10 A src/split.cpp

---
+++ src/split.cpp
@@ -0,0 +1,67 @@
+#include <vector>
+#include <iostream>
+#include <getopt.h>
+#include <regex>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::regex;
+using std::sregex_token_iterator;
+using std::string;
+using std::vector;
+
+static char short_options[] = "s:";
+
+string deli("\\s");
+vector<string> items;
+
+void match(string item, string deli)
+{
+    regex reg("[" + deli + "]");
+    vector<string> v(sregex_token_iterator(item.begin(), item.end(), reg, -1),
+                     sregex_token_iterator());
+    for (auto i : v)
+    {
+        cout << i;
+    }
+    cout << endl;
+}
+
+int main(int argc, char **argv)
+{
+    int c;
+    while (optind < argc)
+    {
+        c = getopt(argc, argv, short_options);
+        switch (c)
+        {
+        case 's':
+            deli = optarg;
+            break;
+
+        case -1:
+            items.push_back(argv[optind++]);
+            break;
+
+        default:
+            return 1;
+        }
+    }
+
+    if (items.size() == 0)
+    {
+        string item;
+        while (cin >> item)
+        {
+            items.push_back(item);
+        }
+    }
+
+    for (auto &item : items)
+    {
+        match(item, deli);
+    }
+
+    return 0;
+}
