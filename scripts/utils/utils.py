bfd99570b2c56ff04f7cafc53e85b4483fdbbf6a
MasterWanna <2467254465@qq.com>
2022-04-23 18:03:04 +0800

    2022-04-23 18:03:04 M scripts/_/bin M scripts/_/pid M scripts/_/readbyte M scripts/_/realpath M scripts/__func__/__splitline__ M scripts/brew/brew-clean M scripts/brew/brew-del M scripts/brew/brew-get M scripts/brew/brew-init M scripts/brew/brew-root A scripts/db/sqlite A scripts/db/sqlite-sort M scripts/git/git-repo M scripts/git/git-repos M scripts/git/git-update M scripts/ip/ip-pub M scripts/utils/utils.py M setup/install M setup/uninstall

--- scripts/utils/utils.py
+++ scripts/utils/utils.py
@@ -1,20 +1,144 @@
+from __future__ import annotations
 import math
 import os
 import re
 from sqlite3 import Connection
-from typing import Dict, Iterable, List, Set, Tuple, Union
+from typing import Any, Dict, Iterable, List, Set, Tuple, Type, Union
+
+# computation utils
 
 
-# utils
-
-
-line_sep = bytes(os.linesep, encoding="UTF-8")[0]
 char_A = b'A'[0]
 char_0 = b'0'[0]
 
 
+def byte_max_len(base: int) -> int:
+    return math.ceil(math.log(255, base))
+
+
+def format_int_base(num: int, base: int) -> str:
+    format_num = ""
+
+    while num != 0:
+        quotient = int(num / base)
+        remainder = num % base
+
+        if remainder > 9:
+            format_num = chr(char_A + remainder - 10) + format_num
+        else:
+            format_num = chr(char_0 + remainder) + format_num
+
+        num = quotient
+
+    return format_num
+
+
+class reverse(object):
+    def __init__(self, obj: Any):
+        self.obj = obj
+
+    def __str__(self) -> str:
+        return "reverse({})".format(self.obj)
+
+    def __repr__(self) -> str:
+        return "reverse({})".format(self.obj)
+
+    def __lt__(self, other: reverse) -> bool:
+        return not self.obj < other.obj
+
+    def __le__(self, other: reverse) -> bool:
+        return not self.obj <= other.obj
+
+    def __eq__(self, other: reverse) -> bool:
+        return self.obj == other.obj
+
+
+class boolean:
+    TRUE = ["true", "t", "1", "yes", "y"]
+
+    def __init__(self, value: Any):
+        if isinstance(value, str):
+            self.value = value.lower() in self.TRUE
+        else:
+            self.value = bool(value)
+
+    def __bool__(self) -> bool:
+        return bool(self.value)
+
+    def __repr__(self) -> str:
+        return str(self.value)
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+
+class convent:
+    def __init__(self, key_sep: str = ',', item_sep: str = ';', item: List[Tuple[bool, Type, Any]] = None, items: int = None) -> None:
+        if item is None and items is None:
+            raise ValueError('item or items must be specified')
+        if key_sep is None:
+            raise ValueError('key_sep must be specified')
+        if item_sep is None:
+            raise ValueError('item_sep must be specified')
+
+        self.key_sep = key_sep
+        self.item_sep = item_sep
+        if item is not None:
+            self.items = len(item)
+            self.item = item
+        else:
+            self.items = items
+            self.item = [(False, str, None)] * items
+
+        assert len(self.item) == self.items, "item must be a list of length items"
+
+        for index in range(self.items):
+            assert len(self.item[index]) == 3, \
+                "item must be a list of length 3"
+
+        for index in range(self.items - 1):
+            assert self.item[index][0] or not self.item[index + 1][0], \
+                "required item must be before optional item"
+
+        self.required = sum([x for x, _, _ in self.item])
+
+    def convert(self, arguments: List[str]) -> List[List[Any]]:
+        elements = []
+        for element in arguments:
+            elements.extend(element.split(self.item_sep))
+
+        all_items: List[str] = [arg.split(self.key_sep) for arg in elements]
+
+        result = []
+
+        for item in all_items:
+            assert self.required <= len(item) <= self.items, \
+                "item must be a list of length less than items"
+
+            item_result = []
+            for index in range(len(item)):
+                item_result.append(self.item[index][1](item[index]))
+
+            for index in range(len(item), self.items):
+                item_result.append(self.item[index][1](self.item[index][2]))
+
+            result.append(item_result)
+
+        return result
+
+    def __call__(self, arguments: List[str]) -> List[List[Any]]:
+        return self.convert(arguments)
+
+
+# command line and terminal utils
+line_sep = bytes(os.linesep, encoding="UTF-8")[0]
+
+
 def get_terminal_width() -> int:
-    return os.get_terminal_size().columns
+    try:
+        return os.get_terminal_size().columns
+    except OSError:
+        return 0
 
 
 def get_realpath(path: str) -> str:
@@ -100,37 +224,26 @@ def get_split_line(splitter: str = '-', text: str = None, length: int = get_term
         return splitter * quotient_l + splitter[:remainder_l] + " " + string + " " + splitter[remainder_l:] + splitter * quotient_r + splitter[:remainder_r]
 
 
-def byte_max_len(base: int) -> int:
-    return math.ceil(math.log(255, base))
+# db utils
 
 
-def format_int_base(num: int, base: int) -> str:
-    format_num = ""
-
-    while num != 0:
-        quotient = int(num / base)
-        remainder = num % base
-
-        if remainder > 9:
-            format_num = chr(char_A + remainder - 10) + format_num
-        else:
-            format_num = chr(char_0 + remainder) + format_num
-
-        num = quotient
-
-    return format_num
+def query_all(conn: Connection, table: str) -> Tuple[List[str], List[Tuple]]:
+    cursor = conn.execute("select * from '{}'".format(table))
+    return [des[0] for des in cursor.description], cursor.fetchall()
 
 
 # brew
 
 
 brew_root_db_path = os.getenv("HOME") + "/.config/brew-formula.db"
+brew_root_table_name = "root-node"
+
 
 # brew database
 
 
 def read_root_nodes(conn: Connection) -> List[str]:
-    cursor = conn.execute("select name from 'root-nodes'")
+    cursor = conn.execute("select name from '{}'".format(brew_root_table_name))
     return [row[0] for row in cursor.fetchall()]
 
 
@@ -139,7 +252,7 @@ def auto_clean_root_nodes(conn: Connection, root_nodes: Union[List[str], Set[str
 
     if len(remove_list) > 0:
         print("Auto clean index : " + to_string(remove_list, sort=True))
-        conn.executemany("delete from 'root-nodes' where name = ?",
+        conn.executemany("delete from '{}' where name = ?".format(brew_root_table_name),
                          [(node,) for node in remove_list])
         conn.commit()
 
@@ -178,12 +291,14 @@ def check_formula_available(name: List[str]) -> int:
 
 
 git_repo_db_path = os.getenv("HOME") + "/.config/git-repo.db"
+git_repo_table_name = "local-repo"
+
 
 # git database
 
 
 def read_local_repos(conn: Connection) -> List[str]:
-    cursor = conn.execute("select path from 'local-repo' order by path")
+    cursor = conn.execute("select path from '{}' order by path".format(git_repo_table_name))
     return [row[0] for row in cursor.fetchall()]
 
 
@@ -221,8 +336,9 @@ def classify_repos(conn: Connection) -> List[str]:
             git_repo_paths.append(path)
 
     if len(git_repo_remove) > 0:
-        print("Remove non-existent git repo : " + to_string(git_repo_remove, sort=True))
-        conn.executemany("delete from 'local-repo' where path = ?",
+        print("Remove non-existent git repo : " +
+              to_string(git_repo_remove, sort=True))
+        conn.executemany("delete from '{}' where path = ?".format(git_repo_table_name),
                          [(path,) for path in git_repo_remove])
         conn.commit()
 
